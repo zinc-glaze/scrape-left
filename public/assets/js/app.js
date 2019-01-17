7a9f465502b8d2af836305a131df259b8985a5de
Zinc Glaze <zekegraves@gmail.com>
2019-01-17 08:40:53 -0500

    DELETE route to remove article

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,9 +1,10 @@
 $(function() {
+
   //Save article on button click
   $(".save-article").on("click", function(event) {
     let articleId = $(this).attr("data-id");
 
-    $.ajax("api/saved/" + articleId, {
+    $.ajax("api/save/" + articleId, {
       type: "PUT",
       data: articleId
     }).then(function() {
@@ -11,4 +12,18 @@ $(function() {
       location.reload();
     });
   });
+
+  //Delete article on button click
+  $(".delete-article").on("click", function(event) {
+    let articleId = $(this).attr("data-id");
+
+    $.ajax("api/delete/" + articleId, {
+      type: "DELETE",
+      data: articleId
+    }).then(function() {
+      console.log("Article" + articleId + "deleted");
+      location.reload();
+    })
+  })
+
 });
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,9 @@ const cheerio = require("cheerio");
 const db = require("../models");
 
 module.exports = function(app) {
+
   //PUT route to update saved status on "save" button click
-  app.put("/api/saved/:id", function(req, res) {
+  app.put("/api/save/:id", function(req, res) {
     //Find article by id and update saved status
     db.Article.update({ _id : req.params.id }, {$set: {saved: true}})
     .then(function(dbArticle) {
@@ -18,4 +19,18 @@ module.exports = function(app) {
       res.json(err);
     });
   })
+
+  //DELETE route to update saved status on "save" button click
+  app.delete("/api/delete/:id", function(req, res) {
+    //Find article by id and update saved status
+    db.Article.remove({ _id : req.params.id })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+  })
+
 };
